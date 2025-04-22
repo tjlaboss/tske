ba86f992bf3518f4f301d0d03a555e3c3e5dd86a
Travis L. <travisl2@illinois.edu>
2025-04-22 14:50:02 -0600

    Import some things

--- tske/__init__.py
+++ tske/__init__.py
@@ -4,6 +4,8 @@ _simplefilter("ignore", UserWarning)
 __author__ = "Travis J. Labossiere-Hickman"
 __email__ = "travisl2@illinois.edu"
 
+from tske.material import Material
+from tske.node import Node1D
 import tske.keys
 import tske.tping
 import tske.modes
