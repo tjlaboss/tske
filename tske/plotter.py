61f4a47e2aa83636c92b77b9efa2dd36550b2e57
Travis L. <travisl2@illinois.edu>
2025-04-24 21:33:03 -0600

    Add 2D plots

--- tske/plotter.py
+++ tske/plotter.py
@@ -11,7 +11,7 @@ import numpy as np
 
 
 # This will make the y-labels not be so stupid.
-rcParams['axes.formatter.useoffset'] = False
+# rcParams['axes.formatter.useoffset'] = False
 
 COLOR_P = "forestgreen"
 COLOR_R = "firebrick"
@@ -46,22 +46,24 @@ def plot_reactivity_and_power(
 	nx, nt = powers.shape
 	assert nt == len(times), \
 		f"The number of times ({len(times)}), powers ({nt}), and reactivities ({nt}) must be equal."
-	
+	xvals = np.arange(nx)
+	pzlims = (min(0.9, 0.9*powers.min()), max(1.1, 1.1*powers.max()))
+	rmin = reacts.min(); rmax = reacts.max()
+	rzlims = (min(1.1*rmin, 0.9*rmin), max(0.9*rmax, 1.1*rmax))
 	# Plot power
-	fig = plt.figure(figsize=[11,5])
+	fig = plt.figure(1, figsize=[11,5])
 	pax = fig.add_subplot(121, projection='3d')
 	rax = fig.add_subplot(122, projection='3d')
-	X, Y = np.meshgrid(np.arange(nx), times)
+	X, Y = np.meshgrid(xvals, times)
 	P = powers.T
 	pax.plot_surface(
 		X, Y, P, edgecolor=COLOR_P, color=COLOR_P,
 		alpha=0.3
 	)
-	pzlims = (0.9*powers.min(), 1.1*powers.max())
 	pax.set(
-		xlim=(0, nx-1),         xlabel="x-node",
+		xlim=(0, nx-1),         xlabel="x (cm)",
 		ylim=(0, max(times)),   ylabel="time (s)",
-		zlim=pzlims,            zlabel="Power"
+		zlim=pzlims,            zlabel="Power",
     )
 	
 	# Plot reactivity
@@ -69,17 +71,44 @@ def plot_reactivity_and_power(
 		X, Y, reacts.T, edgecolor=COLOR_R, color=COLOR_R,
 		alpha=0.3
 	)
-	rzlims = (0.9*reacts.min(), 1.1*reacts.max())
 	rax.set(
-		xlim=(0, nx-1),         xlabel="x-node",
+		xlim=(0, nx-1),         xlabel="x (cm)",
 		ylim=(0, max(times)),   ylabel="time (s)",
-		zlim=rzlims,            zlabel=r"Reactivity (\$)"
+		zlim=rzlims,            zlabel=r"Reactivity (\$)",
 	)
 	
 	# Finish up.
 	fig.tight_layout()
 	if title_text:
 		plt.suptitle(title_text)
+	# plt.tight_layout()
+	
+	# Make 2D plots too
+	fig2 = plt.figure(2, figsize=[11, 5])
+	time_indices = [0]
+	for t in np.ceil( np.logspace(0, np.log(nt-1), 5, base=np.e) ):
+		print(t)
+		time_indices.append(int(t))
+	# times2 = times[time_indices]
+
+	pax2 = fig2.add_subplot(121)
+	rax2 = fig2.add_subplot(122)
+	for t in time_indices:
+		# lbl = fr"t = {times[t]*1e6:.0f} $\mu$s"
+		lbl = fr"t = {times[t]*1e3:.0f} ms"
+		pax2.plot(xvals, powers[:, t], "-",  label=lbl)
+		pax2.set_ylabel(r"$\phi(x)$")
+		pax2.set_ylim(pzlims)
+		
+		rax2.plot(xvals, reacts[:, t], "--", label=lbl)
+		rax2.set_ylabel(r"$\rho(x)$ (\$)")
+		rax2.set_ylim(rzlims)
+		rax2.yaxis.set_label_position("right")
+		rax2.yaxis.tick_right()
+	for ax in (pax2, rax2):
+		ax.legend(loc=0)
+		ax.set_xlim([0, max(times)])
+		ax.set_xlabel("$x$ (cm)")
 	plt.tight_layout()
 
 
