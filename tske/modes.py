3050618b8f4ae7cb232a60256b9745a40645acfe
Travis L. <travisl2@illinois.edu>
2025-04-25 00:14:14 -0600

    Only do plots we need

--- tske/modes.py
+++ tske/modes.py
@@ -30,36 +30,32 @@ def plot_only(output_dir: tske.tping.PathType):
 		le == 0 is OK, le != 0 indicates errors.
 	"""
 	errs = []
-	# Spy plot of Matrix A
-	afpath = os.path.join(output_dir, K.FNAME_MATRIX_A)
-	if not os.path.exists(afpath):
-		errs.append(f"Matrix A could not be found at: {afpath}")
-	else:
-		try:
-			matA = np.loadtxt(afpath)
-			tske.plotter.plot_matrix(matA)
-		except Exception as e:
-			errs.append(f"Failed to plot Matrix A: {type(e)}: {e}")
-		else:
-			plt.savefig(K.FNAME_SPY)
-			print("Matrix A spy plot saved to:", K.FNAME_SPY)
+	# # Spy plot of Matrix A
+	# afpath = os.path.join(output_dir, K.FNAME_MATRIX_A)
+	# if not os.path.exists(afpath):
+	# 	errs.append(f"Matrix A could not be found at: {afpath}")
+	# else:
+	# 	try:
+	# 		matA = np.loadtxt(afpath)
+	# 		tske.plotter.plot_matrix(matA)
+	# 	except Exception as e:
+	# 		errs.append(f"Failed to plot Matrix A: {type(e)}: {e}")
+	# 	else:
+	# 		plt.savefig(K.FNAME_SPY)
+	# 		print("Matrix A spy plot saved to:", K.FNAME_SPY)
 	# Power-Reactivity plot
 	tfpath = os.path.join(output_dir, K.FNAME_TIME)
-	rfpath = os.path.join(output_dir, K.FNAME_RHO)
 	pfpath = os.path.join(output_dir, K.FNAME_P)
 	if not os.path.exists(tfpath):
 		errs.append(f"Times could not be found at: {tfpath}")
-	elif not os.path.exists(rfpath):
-		errs.append(f"Reactivities could not be found at: {rfpath}")
 	elif not os.path.exists(pfpath):
 		errs.append(f"Reactor powers could not be found at: {pfpath}")
 	else:
 		try:
 			times = np.loadtxt(tfpath)
-			reactivities = np.loadtxt(rfpath).T
 			powers = np.loadtxt(pfpath).T
 			# if len(times) != len(reactivities) != len(powers)  -> handled in plotting
-			tske.plotter.plot_reactivity_and_power(times, reactivities, powers)
+			tske.plotter.plot_3d_power(times, powers)
 		except Exception as e:
 			errs.append(f"Failed to plot power and reactivity: {type(e)}: {e}")
 		else:
